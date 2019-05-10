69779aef41ef1c89c8f59002efdabdcbc2a2ea80
chronologos <yiyantay@gmail.com>
2019-05-10 08:22:35 -0700

    remove window snapping

--- hammerspoon/panes.lua
+++ hammerspoon/panes.lua
@@ -44,61 +44,61 @@ end)
 
 hs.window.animationDuration = 0
 --[ window dragging ]----------------------------------------------------------
-function moveWindow(x, y, w, h)
-	local win = hs.window.focusedWindow()
-	local f = win:frame()
-	local screen = win:screen()
-	local max = screen:frame()
-
-	f.x = max.x + (max.w*x)
-	f.y = max.y + (max.h*y)
-	f.w = max.w*w
-	f.h = max.h*h
-	win:setFrame(f)
-end
-dragging_window = nil
-click_event = hs.eventtap.new({hs.eventtap.event.types.leftMouseDragged}, function(e)
-	if dragging_window == nil  then
-		-- check mouse is in titlebar
-		local m = hs.mouse.getAbsolutePosition()
-		local f = hs.window:focusedWindow():frame()
-		local screen = hs.window:focusedWindow():screen()
-		local max = screen:frame()
-		if m.x > f.x and m.x < (f.x + f.w) then
-			if m.y > f.y and m.y < (f.y + 21) then
-				dragging_window = hs.window.focusedWindow()
-				dragging_window_time = hs.timer.localTime()
-			end
-		end
-	end
-end)
-unclick_event = hs.eventtap.new({hs.eventtap.event.types.leftMouseUp}, function(e)
-	if dragging_window ~= nil then
-		local m = hs.mouse.getAbsolutePosition()
-		local f = hs.window:focusedWindow():frame()
-		local screen = hs.window:focusedWindow():screen()
-		local max = screen:frame()
-		if m.x < 50 then
-			if m.y < 200 then
-				moveWindow(0.0,0.0,0.5,0.5)
-			elseif m.y > (max.h-200) then
-				moveWindow(0.0,0.5,0.5,0.5)
-			else
-				moveWindow(0.0,0.0,0.5,1.0)
-			end
-		elseif m.x > (max.w-50) then
-			if m.y < 200 then
-				moveWindow(0.5,0.0,0.5,0.5)
-			elseif m.y > (max.h-200) then
-				moveWindow(0.5,0.5,0.5,0.5)
-			else
-				moveWindow(0.5,0.0,0.5,1.0)
-			end
-		elseif m.y < 25 and m.x>200 and m.x < (max.w-200) then
-				moveWindow(0.0,0.0,1.0,1.0)
-		end
-	end
-	dragging_window = nil
-end)
-click_event:start()
-unclick_event:start()
+-- function moveWindow(x, y, w, h)
+-- 	local win = hs.window.focusedWindow()
+-- 	local f = win:frame()
+-- 	local screen = win:screen()
+-- 	local max = screen:frame()
+--
+-- 	f.x = max.x + (max.w*x)
+-- 	f.y = max.y + (max.h*y)
+-- 	f.w = max.w*w
+-- 	f.h = max.h*h
+-- 	win:setFrame(f)
+-- end
+-- dragging_window = nil
+-- click_event = hs.eventtap.new({hs.eventtap.event.types.leftMouseDragged}, function(e)
+-- 	if dragging_window == nil  then
+-- 		-- check mouse is in titlebar
+-- 		local m = hs.mouse.getAbsolutePosition()
+-- 		local f = hs.window:focusedWindow():frame()
+-- 		local screen = hs.window:focusedWindow():screen()
+-- 		local max = screen:frame()
+-- 		if m.x > f.x and m.x < (f.x + f.w) then
+-- 			if m.y > f.y and m.y < (f.y + 21) then
+-- 				dragging_window = hs.window.focusedWindow()
+-- 				dragging_window_time = hs.timer.localTime()
+-- 			end
+-- 		end
+-- 	end
+-- end)
+-- unclick_event = hs.eventtap.new({hs.eventtap.event.types.leftMouseUp}, function(e)
+-- 	if dragging_window ~= nil then
+-- 		local m = hs.mouse.getAbsolutePosition()
+-- 		local f = hs.window:focusedWindow():frame()
+-- 		local screen = hs.window:focusedWindow():screen()
+-- 		local max = screen:frame()
+-- 		if m.x < 50 then
+-- 			if m.y < 200 then
+-- 				moveWindow(0.0,0.0,0.5,0.5)
+-- 			elseif m.y > (max.h-200) then
+-- 				moveWindow(0.0,0.5,0.5,0.5)
+-- 			else
+-- 				moveWindow(0.0,0.0,0.5,1.0)
+-- 			end
+-- 		elseif m.x > (max.w-50) then
+-- 			if m.y < 200 then
+-- 				moveWindow(0.5,0.0,0.5,0.5)
+-- 			elseif m.y > (max.h-200) then
+-- 				moveWindow(0.5,0.5,0.5,0.5)
+-- 			else
+-- 				moveWindow(0.5,0.0,0.5,1.0)
+-- 			end
+-- 		elseif m.y < 25 and m.x>200 and m.x < (max.w-200) then
+-- 				moveWindow(0.0,0.0,1.0,1.0)
+-- 		end
+-- 	end
+-- 	dragging_window = nil
+-- end)
+-- click_event:start()
+-- unclick_event:start()
